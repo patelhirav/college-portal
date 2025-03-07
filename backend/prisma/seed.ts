0ed150090463915c0040928e6be4ef525d9e3779
patelhirav <patelhirav2212@gmail.com>
2025-03-07 14:43:37 +0530

    update api

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -7,16 +7,17 @@ async function main() {
   const hashedPassword = await bcrypt.hash('1234', 10);
 
   const superAdmin = await prisma.superAdmin.upsert({
-    where: { email: 'hirav.patel@wappzo.com' },
+    where: { email: 'admin@gmail.com' },
     update: {},
     create: {
+      // id: "1",
       name: 'Super Admin',
-      email: 'hirav.patel@wappzo.com',
-      password: hashedPassword, 
+      email: 'admin@gmail.com',
+      password: hashedPassword,
     },
   });
 
-  console.log('✅ Super Admin Seeded:', superAdmin);
+  console.log('Super Admin Seeded:', superAdmin);
 }
 
 main()
--- backend/src/admin/admin.service.ts
+++ backend/src/admin/admin.service.ts
@@ -5,8 +5,8 @@ import {
   BadRequestException,
 } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { SignupDto } from '../dto/signup.dto';
-import { UpdateAdminDto } from '../dto/update-admin.dto';
+import { SignupDto } from '../auth/dto/signup.dto';
+import { UpdateAdminDto } from '../auth/dto/update-admin.dto';
 import * as bcrypt from 'bcrypt';
 import { $Enums } from '@prisma/client';
 
@@ -20,7 +20,6 @@ export class AdminService {
   async addAdmin(adminDto: SignupDto, superAdminId: string) {
     const { name, email, password, branch } = adminDto;
 
-    // Check if the requester is a Super Admin
     const superAdmin = await this.prisma.superAdmin.findUnique({
       where: { id: superAdminId },
     });
@@ -29,17 +28,14 @@ export class AdminService {
       throw new UnauthorizedException('Only Super Admins can add Admins');
     }
 
-    // Check if admin with this email already exists
     const existingAdmin = await this.prisma.admin.findUnique({
       where: { email },
     });
     if (existingAdmin)
       throw new BadRequestException('Admin with this email already exists');
 
-    // Hash the password
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    // Create the Admin
     const admin = await this.prisma.admin.create({
       data: {
         name,
